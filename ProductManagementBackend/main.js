939b492597dd0d67f3f7490c4de596442f0f920d
abdullahkamanger <abdullahkamnger300@gmail.com>
2026-04-10 13:22:33 +0500

    improved

--- ProductManagementBackend/main.js
+++ ProductManagementBackend/main.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import ProductRoutes from './src/Routes/ProductRoutes.js';
-import ConnectDB from './src/Config/db.js';
+import chachu from './src/Config/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-ConnectDB();
+chachu();
 
 app.use(express.json());
 app.use(cors());
